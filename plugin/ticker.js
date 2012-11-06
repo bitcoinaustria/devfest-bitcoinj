af4225010af7a2fad1940a1ea5166988bf72bb22
Harald Schilly <harald.schilly@univie.ac.at>
2012-11-06 15:04:38 +0100

    tiny tweaks; ticker doesnt work - abandoned

--- plugin/ticker.js
+++ plugin/ticker.js
@@ -1,6 +1,26 @@
 /* show current bitcoin price ticker */
 
+/* DOESN'T WORK, no CORS :-( */
+
 (function () {
   var ticker = document.querySelector("#ticker");
-  ticker.innerHTML = "works...";
+
+  return;
+
+  var req = new XMLHttpRequest();
+  req.onreadystatechange = function() {
+    if (req.readyState == 4) {
+      if (req.status == 200) {
+        alert(req.responseText);
+        var data = eval("(" + req.responseText + ")")
+        ticker.innerHTML = data["last"];
+      } else {
+        ticker.innerHTML = "ERROR: " + req;
+      }
+    }
+  }
+
+  req.open("GET", "https://www.bitstamp.net/api/ticker/", true);
+  req.send();
+
 }());
