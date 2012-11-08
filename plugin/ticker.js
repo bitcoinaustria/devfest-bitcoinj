86d752f7925663fd62446390853181de1bbc9534
Harald Schilly <harald.schilly@gmail.com>
2012-11-08 14:55:21 +0100

    ticker: reconnect on error and timeout

--- plugin/ticker.js
+++ plugin/ticker.js
@@ -18,51 +18,57 @@
     ticker.innerHTML = "1Ƀ = " + usd + " " + diff + "@" + trd + " " + trend;
   }
 
-  var conn = io.connect('https://socketio.mtgox.com/mtgox');
+  function connect() {
+    var conn = io.connect('https://socketio.mtgox.com/mtgox');
 
-  /* todo: unsubscribe doesn't work ?! */
-  conn.send({ "op" : "unsubscribe", "channel" : "24e67e0d-1cad-4cc0-9e7a-f8523ef460fe" }); /* depth */
+    /* todo: unsubscribe doesn't work ?! */
+    conn.send({ "op" : "unsubscribe", "channel" : "24e67e0d-1cad-4cc0-9e7a-f8523ef460fe" }); /* depth */
 
-  conn.on('message', function(data) {
-    if (data["channel"] == "d5f06780-30a8-4a48-a2f8-7ed181b4a13f") {
-      if (data["op"] == "private") {
-        var last = data["ticker"]["last"]
-        usd = last["display"];
-        usd_cur = parseFloat(last["value"]);
-        show();
-      } else if (data["op"] == "subscribe") {
-        /* ticker.innerHTML = "connected"; */
-      }
-    } else if (data["channel"] == "dbf1dee9-4f2e-4a08-8cb7-748919a71b21") {
-      if (data["op"] == "private") {
-        var trade = data["trade"];
-        if (trade["price_currency"] == "USD") {
-          trd = "" + trade["amount"].toFixed(3);
-          if (avg == null) { avg = usd_cur; }
-          if (usd_cur > avg + .001)
-             trend = "&#8599;";
-          else if (usd_cur < avg - .001)
-             trend = "&#8600;";
-          else
-             trend = "&rarr;";
+    conn.on('message', function(data) {
+      if (data["channel"] == "d5f06780-30a8-4a48-a2f8-7ed181b4a13f") {
+        if (data["op"] == "private") {
+          var last = data["ticker"]["last"]
+          usd = last["display"];
+          usd_cur = parseFloat(last["value"]);
           show();
-          avg = avg + 0.9 * ( usd_cur - avg );
-          usd_last = usd_cur;
-          /* console.log("avg: " + avg + " cur: " + usd_cur); */
+        } else if (data["op"] == "subscribe") {
+          /* ticker.innerHTML = "connected"; */
+        }
+      } else if (data["channel"] == "dbf1dee9-4f2e-4a08-8cb7-748919a71b21") {
+        if (data["op"] == "private") {
+          var trade = data["trade"];
+          if (trade["price_currency"] == "USD") {
+            trd = "" + trade["amount"].toFixed(3);
+            if (avg == null) { avg = usd_cur; }
+            if (usd_cur > avg + .001)
+               trend = "&#8599;";
+            else if (usd_cur < avg - .001)
+               trend = "&#8600;";
+            else
+               trend = "&rarr;";
+            show();
+            avg = avg + 0.9 * ( usd_cur - avg );
+            usd_last = usd_cur;
+            /* console.log("avg: " + avg + " cur: " + usd_cur); */
+          }
         }
       }
-    }
-  });
+    });
 
-  conn.on("disconnect", function(data) {
-    ticker.innerHTML = "disconnected";
-  });
+    conn.on("disconnect", function(data) {
+      ticker.innerHTML = "disconnected ...";
+      setTimeout(connect, 1000);
+    });
 
-  conn.on("error", function(data) {
-    ticker.innerHTML = "error";
-    console.log("ticker error: " + data);
-  });
+    conn.on("error", function(data) {
+      ticker.innerHTML = "error ...";
+      console.log("ticker error: " + data);
+      setTimeout(connect, 1000);
+    });
 
-  ticker.innerHTML = "...";
+    ticker.innerHTML = "...";
+  }
+
+  connect();
 
 }());
